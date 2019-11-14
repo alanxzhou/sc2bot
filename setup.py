03c1707bf53fe696bb6171e4c403d449ae51634c
Alan <alanzhou93@gmail.com>
2019-11-14 15:26:01 -0500

    notebooks added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sc2bot',
+      version='0.1',
+      description='Starcraft II RL Bot',
+      url='https://github.com/alanxzhou/sc2bot',
+      author='Alan Zhou',
+      author_email='alan.xiang.zhou@gmail.com',
+      license='General Public License',
+      packages=[],
+      zip_safe=False)
