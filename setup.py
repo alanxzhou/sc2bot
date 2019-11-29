a2681bc5c449317e54a038b8cda9264b79b47aa4
alanzhou93 <alanzhou93@gmail.com>
2019-11-29 18:14:10 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='sc2bot',
       version='0.1',
@@ -7,5 +7,5 @@ setup(name='sc2bot',
       author='Alan Zhou',
       author_email='alan.xiang.zhou@gmail.com',
       license='General Public License',
-      packages=['sc2bot'],
+      packages=find_packages(),
       zip_safe=False, install_requires=['pysc2', 'numpy', 'matplotlib', 'torch'])
