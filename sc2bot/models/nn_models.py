a47c7cd8f0df321024d7fdeb6f286fd84e40dc9e
Alan <alanzhou93@gmail.com>
2019-12-10 04:47:29 -0500

    nn_models.py updated

--- sc2bot/models/nn_models.py
+++ sc2bot/models/nn_models.py
@@ -33,14 +33,36 @@ class FeatureCNN(nn.Module):
     """
     def __init__(self, n_features):
         super(FeatureCNN, self).__init__()
-        self.conv1 = nn.Conv2d(n_features, 12, kernel_size=2, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(12, 12, kernel_size=2, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(12, 1, kernel_size=2, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(n_features, 12, kernel_size=3, stride=1, padding=1)  # TODO: Should be 3 to preserve size
+        self.conv2 = nn.Conv2d(12, 12, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(12, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = self.conv3(x)
+        return x
+
+
+class FeatureCNNFC(nn.Module):
+    """
+    CNN model based on feature inputs
+    """
+    def __init__(self, n_features, screen_size=64):
+        super(FeatureCNNFC, self).__init__()
+        self.conv1 = nn.Conv2d(n_features, 12, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(12, 12, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(12, 1, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(screen_size ** 2, screen_size ** 2)
+        self.fc2 = nn.Linear(screen_size ** 2, screen_size ** 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 64 ** 2)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
 
